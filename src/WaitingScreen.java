64910132f1be544ca10391f267eee8cb35f551da
zylooooo <103346969+zylooooo@users.noreply.github.com>
2024-03-26 01:17:21 +0800

    now the waiting screen will only cover the cards of the BottomHand

--- src/WaitingScreen.java
+++ src/WaitingScreen.java
@@ -1,15 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
-public class WaitingScreen extends JOptionPane {    
+public class WaitingScreen extends JOptionPane {   
+    // Set the dimensions of the Frame
+    private final static  int windowHeight = 1200;  
+    private final static int windowWidth = 800;            
+    private final static int borderWidth = 100;      
+    private final static int borderHeight = 100;
+    
     // Constructor for the winning Screen
     public WaitingScreen(JFrame frame) {
         // Create a panel to blur out the parent frame
         JPanel blurPanel = new JPanel();
-        blurPanel.setBackground(new Color(0, 0, 0, 120)); // Semi-transparent black
-        // Set the size of the blur panel to be the same size as the parent frame
-        blurPanel.setSize(frame.getSize());
+        blurPanel.setBackground(new Color(0, 0, 0, 200)); // Semi-transparent black
+
+        // Set the size and location of the blur panel
+        // The parameters are as such: x, y (position of the panel), width, height (size of the panel)
+        blurPanel.setBounds(borderWidth + 80, 560, windowWidth + 30, borderHeight + 100);
 
         // Add the blur panel to the parent frame's layered pane such that it appears right on top of the pane
         frame.getLayeredPane().add(blurPanel, JLayeredPane.PALETTE_LAYER);
@@ -30,12 +37,13 @@ public class WaitingScreen extends JOptionPane {
 
         if (choice == JOptionPane.YES_OPTION) {
             // Continue with the game
+            // Remove the layeredPane of the blur panel
             frame.getLayeredPane().remove(blurPanel);
             frame.revalidate();
             frame.repaint();
         } else {
             // Exit the game
-            frame.dispose();
+            System.exit(0);
         }
     }
 }
